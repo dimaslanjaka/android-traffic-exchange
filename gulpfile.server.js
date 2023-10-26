9db306c4522b10f5e7b2abfa7d122131f7b75a68
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-26 18:22:11 +0700

    fixed static with prefixed base path

--- gulpfile.server.js
+++ gulpfile.server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const gulp = require('gulp');
 const { path, fs } = require('sbg-utility');
+const paths = require('./config/paths');
 const yaml = require('yaml');
 /** @type {typeof import('./_config.json')} */
 const config = yaml.parse(fs.readFileSync(__dirname + '/_config.yml', 'utf-8'));
@@ -9,6 +10,7 @@ gulp.task('serve', () => {
   var app = express();
 
   app.use('/', express.static(path.join(__dirname, config.public_dir)));
+  app.use(paths.base, express.static(path.join(__dirname, config.public_dir)));
 
   app.listen(4000, function () {
     console.log('Production build listening on port 4000!');
