c7294bcc1d72e554d07a5df83e042dbce7a818e3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-26 14:12:26 +0700

    update homepage layout

--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -1,25 +1,28 @@
-import { useEffect, useState } from 'react'
-import ReactMarkdown from 'react-markdown'
-import markdownFile from '@root/readme.md'
-import React from 'react'
+import markdownFile from '@root/readme.md';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
+import './homepage.scss';
 
 const Homepage = () => {
-    const [text, setText] = useState('')
+  //   const [text, setText] = useState('');
 
-    useEffect(() => {
-        // fetch(markdownFile)
-        // .then((response) => response.text())
-        // .then((md) => {
-        //     setText(md)
-        // })
-    }, [])
+  //   useEffect(() => {
+  //     fetch(markdownFile)
+  //       .then(response => response.text())
+  //       .then(md => {
+  //         setText(md);
+  //       });
+  //   }, []);
 
-    return (
-        <div>
-            <h1>Hello world</h1>
-            <ReactMarkdown children={markdownFile} />
-        </div>
-    )
-}
+  return (
+    <div id="homepage" className="markdown-body relative overflow-x-auto">
+      <div className="mx-auto prose lg:prose-lg dark:prose-invert text-gray-500 dark:text-gray-400">
+        <ReactMarkdown children={markdownFile} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} />
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
