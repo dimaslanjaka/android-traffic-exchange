8fe651f9105eb18fe49117ce5bd74f55ad0a2010
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-26 14:44:47 +0700

    install `fancybox`

--- src/components/FlowbiteLayout/index.tsx
+++ src/components/FlowbiteLayout/index.tsx
@@ -6,11 +6,28 @@ import FlowbiteContext from './context/FlowbitContext';
 import { SidebarProvider } from './context/SidebarContext';
 import Header from './header';
 import Sidebar from './sidebar';
+import { Fancybox } from '@fancyapps/ui';
 
 function FlowbiteLayout(props?: { children: JSX.Element | React.ReactNode }) {
-  // React.useEffect(() => {
-  //   darkModeAuto();
-  // });
+  React.useEffect(() => {
+    const wrapper = document.getElementById('router-wrapper');
+    if (wrapper) {
+      wrapper.querySelectorAll('img').forEach(el => {
+        let caption = '';
+        caption += el.getAttribute('title') || '';
+        if (caption.trim().length > 0) caption += ' - ';
+        caption += el.getAttribute('alt') || '';
+        el.setAttribute('data-caption', caption);
+        el.setAttribute('data-fancybox', 'true');
+      });
+      Fancybox.bind(wrapper, '[data-fancybox]', {
+        // Your custom options
+      });
+    }
+    return () => {
+      if (wrapper) Fancybox.unbind(wrapper);
+    };
+  });
 
   return (
     <div id="FlowbiteLayout">
@@ -22,7 +39,7 @@ function FlowbiteLayout(props?: { children: JSX.Element | React.ReactNode }) {
               props.children
             ) : (
               <div className="flex flex-col md:flex-row dark:bg-gray-900">
-                <div className="order-2 mx-4 mt-4 mb-24 flex-[1_0_16rem]">
+                <div className="order-2 mx-4 mt-4 mb-24 flex-[1_0_16rem]" id="router-wrapper">
                   <Outlet />
                 </div>
                 <div className="order-1">
@@ -87,7 +104,7 @@ export function FlowbiteLayoutWithoutSidebar() {
   return (
     <FlowbiteLayout>
       <div className="bg-white dark:bg-gray-900 prose dark:prose-invert max-w-none">
-        <div className="mx-4 text-gray-900 dark:text-white pt-4">
+        <div className="mx-4 text-gray-900 dark:text-white pt-4" id="router-wrapper">
           <Outlet />
         </div>
       </div>
