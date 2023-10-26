c09a5e5eb8cba51cf7fb69634c8e1e4629d6f4c5
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-26 16:51:46 +0700

    fix production build

---
+++ gulpfile.build.js
@@ -0,0 +1,89 @@
+const paths = require('./config/paths');
+const gulp = require('gulp');
+const { generateRouteHtml } = require('./html/generate');
+const { copyPath, fs, array_unique } = require('sbg-utility');
+const yaml = require('yaml');
+const gch = require('git-command-helper');
+/** @type {typeof import('./_config.json')} */
+const config = yaml.parse(fs.readFileSync(__dirname + '/_config.yml', 'utf-8'));
+const args = require('./config/cli');
+
+const configExcludes = array_unique([
+  '.gitignore',
+  /** ignore auto generated post (processed by `sbg post copy`) */
+  '**/_posts/**',
+  /** ignore hexo post data folder */
+  '**/_data/**',
+  /** ignore markdown files (processed by `gulp route`) */
+  '**/*.md',
+  /** ignore dot files */
+  '**/.*',
+  '**/LICENSE',
+  '**/License.md',
+  '**/node_modules/**',
+  '**/readme.md',
+  '**/bin',
+  // vscode settings
+  '**/.vscode',
+  // vscode frontmatter plugin folder
+  '**/.frontmatter',
+  // add exclude from _config.yml
+  ...config.exclude
+]);
+
+// generate html to build dir
+gulp.task('build-html', generateRouteHtml);
+// copy assets to build dir
+gulp.task('build-asset', async () => {
+  // backup dist/index.html
+  const backupIndex = paths.tmp + '/backup/index.html';
+  const sourceIndex = paths.build + '/index.html';
+  await copyPath(sourceIndex, backupIndex);
+  // copy paths.public to paths.build
+  await new Promise((resolve, reject) => {
+    gulp
+      .src('**/*.*', { cwd: paths.public, ignore: ['.gitignore'] })
+      .pipe(gulp.dest(paths.build))
+      .on('end', () => resolve('ends'))
+      .on('error', reject);
+  });
+  // copy /source to paths.build
+  await new Promise((resolve, reject) => {
+    gulp
+      .src('**/*.*', {
+        cwd: paths.cwd + '/source',
+        ignore: configExcludes
+      })
+      .pipe(gulp.dest(paths.build))
+      .on('end', () => resolve('ends'))
+      .on('error', reject);
+  });
+  // restore dist/index
+  await copyPath(backupIndex, sourceIndex);
+});
+
+gulp.task('build', gulp.series('build-html', 'build-asset'));
+
+gulp.task('watch-build', () => {
+  let building = false;
+  const build = async () => {
+    if (!building) {
+      building = true;
+      await gch.spawnAsync('yarn', ['watch:build'], { shell: true, stdio: 'inherit' });
+      building = false;
+    }
+  };
+  let patterns = ['src/**/*.*', 'public/**/*.*', 'source/**/*.*', 'config/**/*.*', 'html/**/*.*'];
+  // change watch files by -p=patterns comma separated
+  if (args.p) patterns = String(args.p).split(',');
+  const watcher = gulp.watch(patterns, {
+    ignored: ['**/node_modules', '**/posts/**', '**/_data/**', '**/_posts/**', '**/post-images/**', '**/tmp/**']
+  });
+  watcher.on('change', (filename, _stat) => {
+    console.log(filename);
+    if (/.(ts|tsx|jsx|js|css|scss)$/.test(filename)) build();
+  });
+  return watcher;
+});
+
+gulp.task('build-watch', gulp.series('watch-build'));
--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,91 @@ require('ts-node').register({
   transpileOnly: true
 });
 
+// const gulp = require('gulp');
 // const { obj } = require('through2');
-// const { path, fs } = require('sbg-utility');
+// const { path, writefile } = require('sbg-utility');
 // /** @type {typeof import('./_config.json')} */
 // const config = yaml.parse(fs.readFileSync(__dirname + '/_config.yml', 'utf-8'));
 require('./gulpfile.config');
-// require('./gulpfile.build');
+require('./gulpfile.build');
 require('./gulpfile.server');
 // require('./gulpfile.page');
 
 // notice webpack file changes
 // by add space to ./src/index.tsx
 // gulp.task('notice', noticeWebpack);
+
+// gulp.task('route', async () => {
+//   const routes = [];
+//   routes.push({
+//     title: 'Website Traffic Exchange For Android',
+//     description: 'Website traffic exchange now available on android',
+//     filename: 'backend/home.html',
+//     meta: {
+//       canonical: {
+//         rel: 'canonical',
+//         href: 'https://www.webmanajemen.com/android-traffic-exchange/backend/home.html'
+//       },
+//       og_image: {
+//         property: 'og:image',
+//         content:
+//           'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTrPydJSDJ0qsdxmme5yXnxWiCKL71LNDgDbdXJxGC31KXn6aG-PyQdmuOFvQ'
+//       },
+//       og_url: {
+//         property: 'og:url',
+//         content: 'https://www.webmanajemen.com/android-traffic-exchange/backend/home.html'
+//       },
+//       og_type: {
+//         property: 'og:type',
+//         content: 'article'
+//       },
+//       twitter_card: {
+//         name: 'twitter:card',
+//         content: 'summary_large_image'
+//       },
+//       twitter_image: {
+//         name: 'twitter:image',
+//         content:
+//           'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTrPydJSDJ0qsdxmme5yXnxWiCKL71LNDgDbdXJxGC31KXn6aG-PyQdmuOFvQ'
+//       },
+//       date: {
+//         property: 'article:published_time',
+//         content: '2023-10-26T16:39:58+07:00'
+//       },
+//       updated: {
+//         property: 'article:modified_time',
+//         content: '2023-10-26T16:39:58+07:00'
+//       },
+//       language: {
+//         httpEquiv: 'Content-Language',
+//         content: 'en'
+//       },
+//       og_locale: {
+//         httpEquiv: 'og:locale',
+//         content: 'en'
+//       },
+//       tags0: {
+//         property: 'article:tag',
+//         content: 'wordpress'
+//       },
+//       tags1: {
+//         property: 'article:tag',
+//         content: 'blogger'
+//       },
+//       tags2: {
+//         property: 'article:tag',
+//         content: 'html'
+//       },
+//       tags3: {
+//         property: 'article:tag',
+//         content: 'blogging'
+//       },
+//       tags4: {
+//         property: 'article:tag',
+//         content: 'programming'
+//       }
+//     },
+//     permalink: 'android-traffic-exchange/backend/home.html'
+//   });
+//   writefile(path.join(__dirname, 'routes.json'), JSON.stringify(routes));
+// });
