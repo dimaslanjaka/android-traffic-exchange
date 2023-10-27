057894d65b883b57f2db580e59e0033709195997
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-27 08:21:39 +0700

    add backend
    Former-commit-id: c8f690d7391d5a34fa3425332d53f9f2001d59f2

---
+++ runtime/chunk/578.180281be962bf4eb2168.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhexo_webpack=self.webpackChunkhexo_webpack||[]).push([[578],{7578:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294);function l(){return r.createElement("div",null,r.createElement("tr",null,r.createElement("td",null,"JS Referer"),r.createElement("td",null,document.referrer)))}}}]);
