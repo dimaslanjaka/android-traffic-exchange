9564b63393955b27c3f2e90b2d921f2d480851c2
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-06 20:21:16 +0700

    update homepage layout

--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -1,21 +1,24 @@
 import { markdown as changelogMd } from '@root/changelog.md';
 import { markdown as readmeMd } from '@root/readme.md';
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 import './homepage.scss';
 
-const Homepage = () => {
-  return (
-    <main id="homepage" className="markdown-body relative overflow-x-auto">
-      <div className="mx-auto prose lg:prose-lg dark:prose-invert text-gray-500 dark:text-gray-400">
-        <ReactMarkdown children={readmeMd} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} />
-        <div style={{ height: '2em' }}></div>
-        <h2 className="text-center">CHANGELOG</h2>
-        <ReactMarkdown children={changelogMd} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} />
-      </div>
-    </main>
-  );
-};
+class Homepage extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <main className="markdown-body">
+        <div className="mx-auto prose lg:prose-lg dark:prose-invert text-gray-500 dark:text-gray-400">
+          <ReactMarkdown children={readmeMd} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} />
+          <div style={{ height: '2em' }}></div>
+          <h2 className="text-center">CHANGELOG</h2>
+          <ReactMarkdown children={changelogMd} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} />
+        </div>
+      </main>
+    );
+  }
+}
 
 export default Homepage;
